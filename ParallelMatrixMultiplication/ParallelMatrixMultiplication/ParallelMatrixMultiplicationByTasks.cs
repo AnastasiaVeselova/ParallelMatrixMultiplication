64b377a241d81abbb4e8d409ca8564115ca6c9e1
Anastasia Veselova <anastasia.v3selova@yandex.ru>
2019-02-23 22:27:33 +0700

    Add matrix multiplication class.

---
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication/ParallelMatrixMultiplicationByTasks.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelMatrixMultiplication
+{
+    public class ParallelMatrixMultiplicationByTasks
+    {
+        public static int[,] MultiplyMatrix(int[,] matrixA, int[,] matrixB)
+        {
+            if (matrixA == null || matrixB == null)
+                throw new ArgumentNullException();
+
+            var aRows = matrixA.GetLength(0);
+            var aColumns = matrixA.GetLength(1);
+
+            var bRows = matrixB.GetLength(0);
+            var bColumns = matrixB.GetLength(1);
+
+            if (aColumns != bRows)
+                throw new ArgumentException("aColumns != bRows");
+
+            var result = new int[aRows, bColumns];
+
+            Task[] tasks = new Task[aRows];
+            for (var i = 0; i < aRows; i++)
+            {
+                var multiplication = new Task(state =>
+                {
+                    var j = (int)state;
+
+                    for (var k = 0; k < bColumns; k++)
+                    {
+                        for (var m = 0; m < aColumns; m++)
+                        {
+                            result[j, k] += matrixA[j, m] * matrixB[m, k];
+                        }
+                    }
+                },
+                i);
+
+                tasks[i] = multiplication;
+                tasks[i].Start();
+            }
+
+            Task.WaitAll(tasks);
+            return result;
+        }
+    }
+}
---
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ParallelMatrixMultiplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ParallelMatrixMultiplication")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("579b4ca9-a6d2-4c80-b6d0-fcecc93cab32")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
