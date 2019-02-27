a8e0465a4ddbea4a590d014949767b68b70361c1
Anastasia Veselova <anastasia.v3selova@yandex.ru>
2019-02-27 14:25:11 +0700

    Исправлено распараллеливание по столбцам.

--- ParallelMatrixMultiplication/ParallelMatrixMultiplication/ParallelMatrixMultiplicationByTasks.cs
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication/ParallelMatrixMultiplicationByTasks.cs
@@ -24,32 +24,34 @@ namespace ParallelMatrixMultiplication
 
             var result = new int[aRows, bColumns];
 
-            var tasks = new Task[aRows * bColumns];
+            var rowTasks = new Task[aRows];
 
             for (var i = 0; i < aRows; i++)
             {
-                for (var j = 0; j < bColumns; j++)
+                var k = i;
+
+                rowTasks[i] = Task.Factory.StartNew(() =>
                 {
-                    var k = i;
-                    var m = j;
+                    var columnTasks = new Task[bColumns];
 
-                    //WaitAll(tasks) - принимает массив тасков, как лучше сделать:
-                    //List<Task> перевести в массив (зачем лист, если мы знаем количетво элементов точное)
-                    //Task[aRows,bColumns] - некрасиво переводить в одномерный, делать Task[aRows][Columns] - тоже нет особого смысла
-                    //а сразу одномерный с вычислением смещения  tasks[i * bColumns + j] - как-то "по-школьному" ?
+                    for (var j = 0; j < bColumns; j++)
+                    {                   
+                        var m = j;
 
-                    tasks[i * bColumns + j] = Task.Factory.StartNew(() =>
-                    {
-                        for (var n = 0; n < aColumns; n++)
+                        columnTasks[j] = Task.Factory.StartNew(() =>
                         {
-                            result[k, m] += matrixA[k, n] * matrixB[n, m];
-                        }
+                            for (var n = 0; n < aColumns; n++)
+                            {
+                                result[k, m] += matrixA[k, n] * matrixB[n, m];
+                            }
 
-                    });
-                }
+                        });
+                    }
+                    Task.WaitAll(columnTasks);
+                });
             }
 
-            Task.WaitAll(tasks);
+            Task.WaitAll(rowTasks);
             return result;
         }
     }
